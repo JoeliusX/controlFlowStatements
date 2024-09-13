031ecb37a16f1cce012576d9fac1090ae5acc1ee
JoeliusX <169055973+JoeliusX@users.noreply.github.com>
2024-09-13 14:40:29 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -3,6 +3,13 @@ for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
+let opperation = 0
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+    opperation++
+    if (numbers[i] > 50) {
+        let reglage = (numbers.length - opperation)
+        console.log("Cette amelioration à reussi à eviter "+reglage)
+        break;
+    }
 }
